2feb3dcdeb1f61493a4efa8701f0efabdfa80085
許誠貿 <chengmaohsu@xuchengmaodeMacBook-Pro.local>
2020-03-05 17:15:16 +0800

    fix: fix publish_npm.sh

--- publish_npm.sh
+++ publish_npm.sh
@@ -1,10 +1,10 @@
 # Usage: 
 # PKG_VERSION=<patch|minor|major> ./publish.sh 
+# For EXAMPLE: PKG_VERSION=1.1.1 ./publish_npm.sh
 
-
+ng build my-lib --prod
+cd dist/my-lib
 npm version $PKG_VERSION -m "v%s"
-# ng build my-lib --prod
-# cd dist/my-lib
-# npm publish --access publish
-# cd ../..
-# npm version $PKG_VERSION -m "v%s"
+npm publish --access publish
+cd ../..
+npm version $PKG_VERSION -m "v%s"
