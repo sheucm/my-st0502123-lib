8e1bf85dadc6fe4bcec18b438985df0c3fa11b5f
許誠貿 <chengmaohsu@xuchengmaodeMacBook-Pro.local>
2020-03-05 17:27:41 +0800

    fix: fix publish_npm.sh

--- publish_npm.sh
+++ publish_npm.sh
@@ -2,9 +2,12 @@
 # PKG_VERSION=<patch|minor|major> ./publish.sh 
 # For EXAMPLE: PKG_VERSION=1.1.1 ./publish_npm.sh
 
+npm list -g | grep json || sudo npm install -g json
 ng build my-lib --prod
 cd dist/my-lib
-npm version $PKG_VERSION -m "v%s"
+json -I -f package.json -e  "this.version='$(echo $PKG_VERSION)'"
+json -I -f package.json -e  "this.name='@mabowco/my-st0502123-lib'"
 npm publish --access publish
 cd ../..
-npm version $PKG_VERSION -m "v%s"
+json -I -f package.json -e  "this.version='$(echo $PKG_VERSION)'"
+json -I -f package.json -e  "this.name='@mabowco/my-st0502123-lib'"
